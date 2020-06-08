7f754bbbe09bdb34fce3f5c60005f793a9a59941
FF220v <san987873@yandex.ru>
2020-06-08 20:38:49 +0300

    network auto config

--- configure_node_network.sh
+++ configure_node_network.sh
@@ -1,6 +1,19 @@
-auto eth0
-iface eth0 inet static
-    address 10.0.0.6
-    netmask 255.255.255.0
-    gateway 10.0.0.1
-    dns-nameservers
+NODE_NUMBER=$1
+
+IFACE_CONFIG="\
+source /etc/network/interfaces.d/* \n \
+auto lo \n \
+iface lo inet loopback \n \
+auto eth0 \n \
+ iface eth0 inet static \n \
+     address 10.0.0.$((NODE_NUMBER + 1)) \n \
+     netmask 255.255.255.0 \n \
+     gateway 10.0.0.1 \n \
+     dns-nameservers 10.0.0.1"
+
+DNS_CONFIG="nameserver 10.0.0.1" 
+
+sudo echo DNS_CONFIG > /etc/resolv.conf 
+sudo echo IFACE_CONFIG > /etc/network/interfaces 
+hostnamectl set-hostname node${NODE_NUMBER}
+
--- install_node.sh
+++ install_node.sh
@@ -1,2 +1,3 @@
+sudo apt-get install nano
 sudo apt-get install snap
 sudo snap install microk8s --classic
