09671cfaeff50e4c8271c45f472f2b59c547f852
FF220v <san987873@yandex.ru>
2020-06-08 23:27:36 +0300

    more cool sh

--- install_snap_on_nodes.sh
+++ install_snap_on_nodes.sh
@@ -1,7 +1,7 @@
 
 for i in {1..6}
 do
-        ssh root@node${i}.cluster.io "apt-get install snap"
+        ssh root@node${i}.cluster.io "apt-get install -y snap && apt-get -y install snapd"
 done
 
 
